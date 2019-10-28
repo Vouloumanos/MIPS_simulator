1a5cb6970208deb45d2d8e26f63e39cedd613b91
Filip Wojcicki <fiwo735@gmail.com>
2019-10-28 19:02:20 +0000

    Update simulator.cpp
    initial design from panopto

--- simulator.cpp
+++ simulator.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <iterator>
 
 
 
-int main(){
+
+int main(int argc, char *argv[]){
+
+  //get binary name
+  std::string binName = argv[1];
+  //debug - display binary name
+  std::cerr << "binName: " << binName << std::endl;
+  //initialise binStream
+  std::ifstream binStream(binName, std::ios::binary);
+
+  std::vector<uint32_t> buffer(std::istream_iterator<uint32_t>(binStream), {});
+
+
+
+
 
   return 0;
 }
