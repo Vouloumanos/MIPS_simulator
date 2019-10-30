441e481d38065823f0aa16350239e7610f25afbe
Filip Wojcicki <fiwo735@gmail.com>
2019-10-30 16:48:19 +0000

    new class for instruction R

---
+++ instruction_R.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bitset>
+#include "instruction_R.hpp"
+
+void instruction_R::set_bits(const uint32_t& input_bits){
+  bits = input_bits;
+  opcode = (bits >> 26);
+  src1 = (bits >> 21);
+  src2 = (bits >> 16);
+  dest = (bits >> 11);
+  shift_amount = (bits >> 6);
+  fn_code = (bits >> 0);
+}
---
+++ instruction_R.hpp
@@ -0,0 +1,20 @@
+#ifndef INSTRUCTION_R_HPP
+#define INSTRUCTION_R_HPP
+
+#include <bitset>
+
+class instruction_R{
+public:
+  void set_bits(const uint32_t& input_bits);
+
+private:
+  bitset<32> bits;
+  bitset<6> opcode;
+  bitset<5> src1;
+  bitset<5> src2;
+  bitset<5> dest;
+  bitset<5> shift_amount;
+  bitset<6> fn_code;
+};
+
+#endif
