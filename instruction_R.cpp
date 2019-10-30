f35ac357f11d5df64cf37ac7cb71e31ec9b35796
Filip Wojcicki <fiwo735@gmail.com>
2019-10-30 19:03:32 +0000

    Added skeleton for all R functions

--- instruction_R.cpp
+++ instruction_R.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bitset>
+#include <vector>
 #include "instruction_R.hpp"
 
 void instruction_R::set_bits(const uint32_t& input_bits){
@@ -14,6 +15,107 @@ void instruction_R::set_bits(const uint32_t& input_bits){
 
 void instruction_R::execute(std::vector<uint32_t>& registers, uint32_t& pc){
   switch(fn_code){
-    case
+    case 0b100000: ADD(registers);
+    case 0b100001: ADDU(registers);
+    case 0b100100: AND(registers);
+    case 0b011010: DIV(registers);
+    case 0b011011: DIVU(registers);
+    case 0b001001: JALR(registers, pc);
+    case 0b001000: JR(registers, pc);
+    case 0b010000: MFHI(registers);
+    case 0b010010: MFLO(registers);
+    case 0b010001: MTHI(registers);
+    case 0b010011: MTLO(registers);
+    case 0b011000: MULT(registers);
+    case 0b011001: MULTU(registers);
+    case 0b100101: OR(registers);
+    case 0b000000: SLL(registers);
+    case 0b000100: SLLV(registers);
+    case 0b101010: SLT(registers);
+    case 0b101011: SLTU(registers);
+    case 0b000011: SRA(registers);
+    case 0b000111: SRAV(registers);
+    case 0b000010: SRL(registers);
+    case 0b000110: SRLV(registers);
+    case 0b100010: SUB(registers);
+    case 0b100011: SUBU(registers);
+    case 0b100110: XOR(registers);
   }
+  if(fn_code != 0b001001 && fn_code != 0b001000) pc += 4;
+}
+
+void instruction_R::ADD(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::ADDU(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::AND(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::DIV(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::DIVU(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::JALR(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::JR(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::MFHI(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::MFLO(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::MTHI(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::MTLO(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::MULT(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::MULTU(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::OR(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SLL(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SLLV(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SLT(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SLTU(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SRA(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SRAV(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SRL(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SRLV(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SUB(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::SUBU(std::vector<uint32_t>& registers, uint32_t& pc){
+
+}
+void instruction_R::XOR(std::vector<uint32_t>& registers, uint32_t& pc){
+
 }
--- instruction_R.hpp
+++ instruction_R.hpp
@@ -2,11 +2,38 @@
 #define INSTRUCTION_R_HPP
 
 #include <bitset>
+#include <vector>
 
 class instruction_R{
 public:
   void set_bits(const uint32_t& input_bits);
   friend void execute(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void ADD(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void ADDU(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void AND(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void DIV(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void DIVU(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void JALR(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void JR(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void MFHI(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void MFLO(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void MTHI(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void MTLO(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void MULT(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void MULTU(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void OR(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SLL(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SLLV(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SLT(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SLTU(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SRA(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SRAV(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SRL(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SRLV(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SUB(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void SUBU(std::vector<uint32_t>& registers, uint32_t& pc);
+  friend void XOR(std::vector<uint32_t>& registers, uint32_t& pc);
+
 
 private:
   std::bitset<32> bits;
