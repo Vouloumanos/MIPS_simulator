cea34c11ab6cdc543e7a3fc64f87d8f030f8c60f
Fiwo735 <fiwo735@gmail.com>
2019-11-17 15:44:40 +0000

    commented out cerr

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -22,11 +22,11 @@ int main(int argc, char *argv[]){
     std::vector<uint32_t> registers(34, 0); //initialiste registers to 0, registers[32] is LO, registers[33] is HI
     uint32_t pc = IMEM_OFFSET; //initialise program counter to IMEM_OFFSET
     uint32_t next_pc = IMEM_OFFSET + 4; //initialise next program counter to
-    std::cerr << "Everything initialised " << std::endl; //debug - status update
+    //std::cerr << "Everything initialised " << std::endl; //debug - status update
 
     //set up binstream
     std::string binName = argv[1]; //get binary name
-    std::cerr << "binName: " << binName << std::endl; //debug - display binary name
+    //std::cerr << "binName: " << binName << std::endl; //debug - display binary name
     std::ifstream binStream(binName, std::ios::binary); //initialise binStream
 
     if(!binStream.is_open()){ //THINK ABOUT CORRECT ERROR CODE
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]){
 
     binStream.seekg(0, std::ios::end); //calculate binSize
     int binSize = binStream.tellg();
-    std::cerr << "Binary file size: " << binSize << std::endl;
+    //std::cerr << "Binary file size: " << binSize << std::endl;
 
     binStream.seekg(0, std::ios::beg);
     char* buffer = new char[binSize];
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]){
     }
     delete[] buffer;
 
-    std::cerr << "Input memory OK" << std::endl; //debug - status message
+    //std::cerr << "Input memory OK" << std::endl; //debug - status message
 
     while(1){ //processor running
 
@@ -65,38 +65,38 @@ int main(int argc, char *argv[]){
         throw(static_cast<int32_t>(exception::MEMORY));
       }
       else if((pc < IMEM_LENGTH + IMEM_OFFSET) && (pc >= IMEM_OFFSET)){
-        std::cerr << "Valid pc" << std::endl;
+        //std::cerr << "Valid pc" << std::endl;
 
         //get instruction
         uint32_t input_bits = (uint32_t(memory[pc]) << 24) + (uint32_t(memory[pc+1]) << 16) + (uint32_t(memory[pc+2]) << 8) + (uint32_t(memory[pc+3]) << 0);
-        std::cerr << "instruction: " << std::bitset<32> (input_bits) << std::endl;//debug - status
+        //std::cerr << "instruction: " << std::bitset<32> (input_bits) << std::endl;//debug - status
 
         //store next_pc temporarily as it will get changed during execution
         uint32_t temp_next_pc = next_pc;
 
         //execute instruction depending on the type
         if(get_type(input_bits) == 'R'){
-          std::cerr << "instruction R detected" << std::endl;
+          //std::cerr << "instruction R detected" << std::endl;
           instruction_R inst;
           inst.set_bits(input_bits);
           inst.execute(registers, pc, next_pc);
-          std::cerr << "instruction R executed" << std::endl;
+          //std::cerr << "instruction R executed" << std::endl;
 
         }
         else if(get_type(input_bits) == 'I'){
-          std::cerr << "instruction I detected" << std::endl;
+          //std::cerr << "instruction I detected" << std::endl;
           instruction_I inst;
           inst.set_bits(input_bits);
           inst.execute(registers, pc, next_pc, memory);
-          std::cerr << "instruction I executed" << std::endl;
+          //std::cerr << "instruction I executed" << std::endl;
 
         }
         else if(get_type(input_bits) == 'J'){
-          std::cerr << "instruction J detected" << std::endl;
+          //std::cerr << "instruction J detected" << std::endl;
           instruction_J inst;
           inst.set_bits(input_bits);
           inst.execute(registers, pc, next_pc);
-          std::cerr << "instruction J executed" << std::endl;
+          //std::cerr << "instruction J executed" << std::endl;
         }
 
         registers[0] = 0; // register 0 is grounded and cannot hold value other than 0
