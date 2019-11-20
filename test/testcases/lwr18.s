548473b45289766cd391d3a2c3c0547862fb96b3
Vouloumanos <sv1818@ic.ac.uk>
2019-11-20 23:26:49 +0000

    lwr18

--- test/testcases/lwr18.s
+++ test/testcases/lwr18.s
@@ -2,9 +2,9 @@ lui $3, 0x1000
 ori $3, $3, 0x000C
 lui $4, 0xAABB
 ori $4, $4, 0xCCDD
-lui $5, 0xEEEE
-ori $5, $5, 0xEEEE
-lwr $5, 2($3)
+lui $2, 0xEEEE
+ori $2, $2, 0xEEEE
+lwr $2, 2($3)
 jr $zero
 nop
 
