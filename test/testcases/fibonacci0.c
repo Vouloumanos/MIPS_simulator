d0e90c228c6b6a8f6153caf27c52d78fc2d30375
Fiwo735 <fiwo735@gmail.com>
2019-11-21 23:59:44 +0000

    changes to fibonacci

--- test/testcases/fibonacci0.c
+++ test/testcases/fibonacci0.c
@@ -1,9 +1,9 @@
 asm("li $sp, 0x23FFFFFC");
 
-// int fibonacci(int n);
+int fibonacci(int n) __attribute__ ((destructor));
 
 //int main();
-int add();
+//int add();
 
 int main(){
 
@@ -13,17 +13,18 @@ int main(){
 	//return 55;
 	return add();
 }
-// int fibonacci(int n){
-// 	if(n == 0){
-// 		return 0;
-// 	}
-// 	else if(n == 1){
-// 		return 1;
-// 	}
-// 	return (fibonacci(n-1) + fibonacci(n-2));
-// }
 
-
-int add(){
-return 7;
+int fibonacci(int n){
+	if(n == 0){
+		return 0;
+	}
+	else if(n == 1){
+		return 1;
+	}
+	return (fibonacci(n-1) + fibonacci(n-2));
 }
+
+
+// int add(){
+// return 7;
+// }
