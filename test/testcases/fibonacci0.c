cc4efc8e83fa5cc65816068978a68114542ef313
Fiwo735 <fiwo735@gmail.com>
2019-11-21 23:49:26 +0000

    bugfixes

--- test/testcases/fibonacci0.c
+++ test/testcases/fibonacci0.c
@@ -1,21 +1,29 @@
-
-int main();
-int fibonacci(int n);
 asm("li $sp, 0x23FFFFFC");
+
+// int fibonacci(int n);
+
+//int main();
+int add();
 
 int main(){
 
-	//int n = 10;
-	//return fibonacci(n);
-	return 55;
+	// int n = 10;
+	// int result = fibonacci(n);
+	// asm("")
+	//return 55;
+	return add();
 }
+// int fibonacci(int n){
+// 	if(n == 0){
+// 		return 0;
+// 	}
+// 	else if(n == 1){
+// 		return 1;
+// 	}
+// 	return (fibonacci(n-1) + fibonacci(n-2));
+// }
 
-int fibonacci(int n){
-	if(n == 0){
-		return 0;
-	}
-	else if(n == 1){
-		return 1;
-	}
-	return (fibonacci(n-1) + fibonacci(n-2));
+
+int add(){
+return 7;
 }
