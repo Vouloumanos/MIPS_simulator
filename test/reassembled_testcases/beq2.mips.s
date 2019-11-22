9e1af4b4bc668f12f6d80e288075b75ee9b25659
Fiwo735 <fiwo735@gmail.com>
2019-11-22 15:46:15 +0000

    io

---
+++ test/reassembled_testcases/beq2.mips.s
@@ -0,0 +1,15 @@
+
+./test/temp/beq2.mips.elf:     file format elf32-tradbigmips
+
+
+Disassembly of section .text:
+
+10000000 <start>:
+10000000:	24020005 	li	v0,5
+10000004:	24030005 	li	v1,5
+10000008:	10430004 	beq	v0,v1,1000001c <start+0x1c>
+1000000c:	00000000 	nop
+10000010:	00431020 	add	v0,v0,v1
+10000014:	00000008 	jr	zero
+10000018:	00000000 	nop
+1000001c:	1043fffd 	beq	v0,v1,10000014 <start+0x14>
