f8ab243a3ba6b8629d1a9c84ab6bab8342112165
Vouloumanos <sv1818@ic.ac.uk>
2019-11-22 15:29:49 +0000

    out

---
+++ test/testcases/beq2.s
@@ -0,0 +1,14 @@
+.set noreorder
+
+start:
+  addiu $2, $0, 5
+  addiu $3, $0, 5
+  beq $2, $3, start + 28
+  nop
+  add $2, $2, $3
+  jr $0
+  nop
+
+  beq $2, $3, start + 20
+
+#Check for not branching. Expected: 11
