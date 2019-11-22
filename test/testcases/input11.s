6ced246c6e4f8ab6e8d076b6a3c6ef82b36c7916
Vouloumanos <sv1818@ic.ac.uk>
2019-11-22 03:08:50 +0000

    io

--- test/testcases/input11.s
+++ test/testcases/input11.s
@@ -1,6 +1,8 @@
 lui $3, 0x3000
+lw $4, 0($3)
 lw $2, 0($3)
+srl $2, $2, 24
 jr r0
 nop
 
-#Load character from input
+#EOF
--- test/testcases/input8.s
+++ test/testcases/input8.s
@@ -1,6 +1,7 @@
 lui $3, 0x3000
+lw $4, 0($3)
 lw $2, 0($3)
 jr r0
 nop
 
-#Load character from input
+#EOF
