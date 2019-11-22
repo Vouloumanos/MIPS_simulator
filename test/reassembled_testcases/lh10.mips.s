4a25122500616b0a92eefe0f4d1655ae08425a71
Fiwo735 <fiwo735@gmail.com>
2019-11-22 02:07:09 +0000

    results update

--- test/reassembled_testcases/lh10.mips.s
+++ test/reassembled_testcases/lh10.mips.s
@@ -9,7 +9,7 @@ Disassembly of section .text:
 10000004:	3463000c 	ori	v1,v1,0xc
 10000008:	3c04aabb 	lui	a0,0xaabb
 1000000c:	3484ccdd 	ori	a0,a0,0xccdd
-10000010:	84620000 	lh	v0,0(v1)
+10000010:	84620002 	lh	v0,2(v1)
 10000014:	00000000 	nop
 10000018:	00000008 	jr	zero
 1000001c:	00021202 	srl	v0,v0,0x8
